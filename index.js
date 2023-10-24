348eb43241ba2d9be54f53369aec6a158d96d456
LL <liaposhchenkol@dmail.com>
2023-10-24 15:26:13 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('l');
+const now = dayjs();
+const fiftiethAnniversary = dayjs('2024-08-08');
+
+if (now.isAfter(fiftiethAnniversary)) {
+  document.body.innerHTML += 'yes';
+} else {
+  document.body.innerHTML += 'no';
+}
